a315067b7c7a9f7df3b25158b0332d30fb945f69
mca <abutimartin778@gmail.com>
2025-06-24 02:31:29 +0300

    c1

---
+++ isp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpRequest
+from inertia import render
+
+
+def index(request):
+    return render(request, "Index", props={"name": "World"})
+
+
+def about(request):
+    return render(request, "About", props={"pageName": "About"})
