de8fc2c3d52e01fff9917144c5d6afdd74809542
mca <abutimartin778@gmail.com>
2025-10-17 01:57:36 +0300

    003

--- setup-production.sh
+++ setup-production.sh
@@ -463,7 +463,7 @@ server {
 
     # Static files
     location /static/ {
-        alias $APP_DIR/static/;
+        alias $APP_DIR/isp/static/dist/;
         expires 1y;
         add_header Cache-Control "public, immutable";
     }
